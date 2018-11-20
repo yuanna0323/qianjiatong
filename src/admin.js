81fe15e1b85199fa000cd71cef2637962d4f81a4
15010611339@163.com <15010611339@163.com>
2018-11-20 16:26:32 +0800

    引入antd(蚂蚁金融)，添加结构组件并引入

---
+++ src/admin.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default class Admin extends React.Component {
+
+    render() {
+        return (
+            <Row>
+                <Col span="3">Left</Col>
+                <Col span="21">
+                    <Header />
+                    <Row>
+
+                    </Row>
+                    <Footer />
+                </Col>
+            </Row>
+        )
+    }
+}
