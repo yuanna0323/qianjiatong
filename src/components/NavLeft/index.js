c86ebfd27ed854e6d98a86733d602e47c45737db
15010611339@163.com <15010611339@163.com>
2018-11-21 10:48:24 +0800

    左侧导航引入antd

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import MenuConfig from './../../config/menuConfig'
+import { Menu, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 export default class NavLeft extends React.Component {
 
     render() {
         return (
             <div>
-                NavLeft
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" />
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu>
+                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </SubMenu>
+                </Menu>
             </div>
         )
     }
