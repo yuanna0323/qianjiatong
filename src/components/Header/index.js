7c06cece5217444eb93457e0da36cbb6ff320d84
15010611339@163.com <15010611339@163.com>
2018-11-21 17:44:26 +0800

    (添加头部的时间和资源)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
-
+import { Row, Col } from 'antd'
+import './index.css'
+import Util from '../../utils/utils'
 export default class Header extends React.Component {
-
+    state = {}
+    componentWillMount() {
+        this.setState({
+            userName: '河畔一角'
+        })
+        setInterval(() => {
+            let sysTime = Util.formateDate(new Date().getTime());
+            this.setState({
+                sysTime
+            })
+        }, 1000)
+    }
     render() {
         return (
-            <div>
-                header
+            <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span="20" className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-detail">晴转多云</span>
+                    </Col>
+                </Row>
             </div>
         )
     }
